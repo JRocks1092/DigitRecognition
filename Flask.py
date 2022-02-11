3b76dd3bfe2d4bd9e4904ce8285b399ce8646c2d
JRocks 1092 <74896006+JRocks1092@users.noreply.github.com>
2022-02-11 18:01:15 +0530

    Add files via upload

---
+++ Flask.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify,request
+from API import recogniseNumber
+
+app = Flask(__name__)
+
+@app.route("/getNumber")
+def getNumber():
+    res = request.args.get("name")
+    number = recogniseNumber()
+
+    return jsonify({
+        "predicted Number":number
+        }),200
+        
+if __name__ == "__main__": 
+    app.run(debug=True)
